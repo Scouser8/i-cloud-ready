70f1435b834908566ab4e7c060042baa6681f13f
unknown <mohammed.abdelbaki96@hotmail.com>
2023-11-24 23:08:42 +0300

    Add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "fixed",
+        left: 0,
+        bottom: 0,
+        width: "100%",
+        height: "30px",
+      }}
+    >
+      Got questions? Take a look at our FAQs, talk to us on Twitter @company or
+      send an email to team@company.com
+    </div>
+  );
 }
