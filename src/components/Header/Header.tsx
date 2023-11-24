481300c9708b2cf84821962bd51f9f041134cf2e
unknown <mohammed.abdelbaki96@hotmail.com>
2023-11-24 18:14:52 +0300

    Implement Header main parts & will be enhanced later

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,28 @@
-import React from "react";
 import styles from "@/styles/Header.module.css";
 import { Typography } from "antd";
+import { Avatar, Badge } from "antd";
+import { ShopFilled } from "@ant-design/icons";
 
 export default function Header() {
-  return <div className={styles.root}>Header</div>;
+  return (
+    <div className={styles.root}>
+      <div className={styles.welcomeTitle}>
+        <Typography className={styles.welcomeText}>
+          Welcome to My Company!
+        </Typography>
+        <Typography className={styles.welcomeTextInfo}>
+          You have started your 30 day trial
+        </Typography>
+      </div>
+      <div className={styles.headerInfo}>Some Info</div>
+      <div className={styles.actionButtons}>
+        <Badge count={2} showZero>
+          <Avatar size="large" icon={<ShopFilled />} />
+        </Badge>
+        <Badge count={2} showZero>
+          <Avatar size="large" icon={<ShopFilled />} />
+        </Badge>
+      </div>
+    </div>
+  );
 }
